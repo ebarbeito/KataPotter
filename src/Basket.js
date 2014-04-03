af7ff0e4b379e034b36da9ac753634f6dfa597da
Luis Rovirosa <luisrovirosa@gmail.com>
2014-04-03 19:42:59 +0200

    Basket refactoring to solve the "first", "first", "second" books in the basket

--- src/Basket.js
+++ src/Basket.js
@@ -1,32 +1,39 @@
 function Basket() {
-  this.books = [];
+  this.books = {};
+  this.numBooks = 0;
 }
 
 Basket.prototype.addBook = function(book) {
-  this.books.push(book);
+  if (!this.books[book.title]) {
+    this.books[book.title] = 0;
+  }
+  this.books[book.title] += 1;
+  this.numBooks++;
 };
 
 Basket.prototype.price = function() {
   var differentBookDiscounts = {
+    0: 0,
+    1: 0,
     2: 0.05,
     3: 0.1
   };
-  var price = this.books.length * 8;
+  var price = this.numBooks * 8;
   var discount = 0;
   if (areDifferent(this.books)) {
-    discount = differentBookDiscounts[this.books.length] * price;
+    discount = differentBookDiscounts[this.numBooks] * price;
   }
   return price - discount;
 }
 ;
 
 var areDifferent = function(books) {
-  if (books.length > 1) {
-    return books[0].title !== books[1].title;
-  } else {
-    return false;
+  for (var title in books) {
+    if (books[title] !== 1) {
+      return false;
+    }
   }
-
+  return true;
 };
 
 module.exports.Basket = Basket;
