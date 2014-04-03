2c572e29228d96970c63269953fab3a53ca85ee2
Luis Rovirosa <luisrovirosa@gmail.com>
2014-04-03 20:12:43 +0200

    when the basket has 2 "first" books and 2 "second" books the price has to apply twice 5% of discount

--- src/Basket.js
+++ src/Basket.js
@@ -18,14 +18,32 @@ Basket.prototype.price = function() {
   };
   var price = this.numBooks * 8;
   var discount = 0;
-  var numBooks = numDifferentBooks(this.books);
-  if (numBooks > 1) {
-    discount = differentBookDiscounts[numBooks] * 8 * numBooks;
+  var localBooks = this.books;
+  while (hasBooks(localBooks)) {
+    var numBookOfPack = sizeOfPack(localBooks);
+    if (numBookOfPack > 1) {
+      discount += differentBookDiscounts[numBookOfPack] * 8 * numBookOfPack;
+    }
+    localBooks = removePack(localBooks);
   }
   return price - discount;
 };
 
-var numDifferentBooks = function(books) {
+var hasBooks = function(books) {
+  return sizeOfPack(books) > 0;
+};
+
+var removePack = function(books) {
+  for (var title in books) {
+    books[title]--;
+    if (books[title] === 0) {
+      delete books[title];
+    }
+  }
+  return books;
+};
+
+var sizeOfPack = function(books) {
   return Object.keys(books).length;
 };
 
