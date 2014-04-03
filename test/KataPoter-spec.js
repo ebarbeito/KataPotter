f4d23cb5faed322f949cbc240f0f177f5763d5d5
Enrique Barbeito García <ebarbeito@gmail.com>
2014-04-03 17:29:47 +0200

    test refactoring

--- test/KataPoter-spec.js
+++ test/KataPoter-spec.js
@@ -2,14 +2,17 @@ var Basket = require('../src/Basket.js').Basket;
 var Book = require('../src/Book.js').Book;
 
 describe('KataPoter', function() {
+  var basket;
+  
+  beforeEach(function() {
+    basket = new Basket();
+  });
 
   it('an empty basket book costs 0', function() {
-    var basket = new Basket();
     expect(basket.price()).toBe(0);
   });
 
   it('A basket with 1 book costs 8 €', function() {
-    var basket = new Basket();
     var firstBook = new Book("First book");
     basket.addBook(firstBook);
     expect(basket.price()).toBe(8);
